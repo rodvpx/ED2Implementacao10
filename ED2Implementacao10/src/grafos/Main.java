5205ecbdcf5402a192b5027ae964d6e251021384
Rodrigo Simão <rodrigoip435@gmail.com>
2024-12-09 18:15:47 -0300

    funcionando 1

--- ED2Implementacao10/src/grafos/Main.java
+++ ED2Implementacao10/src/grafos/Main.java
@@ -3,13 +3,16 @@ package grafos;
 public class Main {
 
 	public static void main(String[] args) {
-		// Criação do grafo com 16 vértices (10 professores + 6 disciplinas)
-		Grafo<String> grafo = new Grafo<>(16);
+
+		// Criação do grafo com 24 vértices (9 professores + 15 disciplinas)
+		Grafo<String> grafo = new Grafo<>(24);
 
 		// Adicionando professores
-		String[] professores = {"Nattane", "Lucas", "Cristiane", "Junio", "Jean", "Paulo", "Júlio", "Aumaury", "Gabriel", "Marcio"};
+		String[] professores = {"Nattane", "Lucas", "Cristiane", "Junio", "Jean", "Paulo", "Júlio", "Aumaury", "Gabriel"};
 		String[] disciplinas = {"Desenvolvimento Web I", "Álgebra Linear", "Banco de Dados II", "Estrutura de Dados II",
-				"Redes de Computadores", "Metodologia Científica"};
+				"Redes de Computadores", "Metodologia Científica", "Programação II", "Elaboração de Projetos",
+				"Programação Mobile", "Ética", "Eng. de Software", "Lógica", "Sistemas Operacionais",
+				"Arquitetura de Software", "Marketing e Inovação"};
 
 		// Adicionando os professores e disciplinas no grafo
 		for (String professor : professores) {
@@ -35,13 +38,22 @@ public class Main {
 		grafo.addAresta(1, "Júlio", "Lógica");
 		grafo.addAresta(1, "Aumaury", "Sistemas Operacionais");
 		grafo.addAresta(1, "Gabriel", "Arquitetura de Software");
-		grafo.addAresta(1, "Marcio", "Marketing e Inovação");
+		grafo.addAresta(1, "Lucas", "Marketing e Inovação");
 
-		// Atualização dos pesos (se necessário)
-		grafo.atualizarPeso("Nattane", "Desenvolvimento Web I", 3);
 
-		// Obter matriz de custos
-		int[][] costMatrix = grafo.getMatrizDeCustos();
+
+		// Matriz de custos
+		int[][] costMatrix = {
+				{2, 10, 2, 10, 10, 1, 10, 10, 10, 10, 10, 10, 10, 10, 2}, // Nattane
+				{10, 0, 10, 10, 3, 10, 10, 10, 10, 10, 5, 10, 10, 10, 10}, // Lucas
+				{10, 1, 2, 10, 10, 2, 1, 2, 10, 10, 10, 4, 10, 10, 10}, // Cristiane
+				{10, 1, 10, 1, 10, 10, 10, 10, 1, 10, 10, 10, 2, 10, 6}, // Junio
+				{10, 10, 10, 10, 1, 10, 10, 10, 10, 1, 10, 10, 10, 10, 10}, // Jean
+				{10, 3, 10, 10, 10, 1, 10, 10, 10, 10, 1, 10, 10, 10, 10}, // Paulo
+				{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 1, 10, 10, 10}, // Júlio
+				{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 1, 10, 10}, // Aumaury
+				{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 1, 10}, // Gabriel
+		};
 
 		// Execução do Algoritmo Húngaro
 		AlgoritimoHungaro hungaro = new AlgoritimoHungaro();
